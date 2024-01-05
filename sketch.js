6c3897e165448ab26ad710e4a74d8868ba03384d
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-01-04 19:30:45 -0600

    added flow field

--- sketch.js
+++ sketch.js
@@ -1,8 +1,60 @@
+var inc = 0.05;
+var scl = 10;
+
+var cols, rows;
+var zoff = 0;
+var fr;
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  createCanvas(400, 400);
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+
+  fr = createP('');
 }
 
 function draw() {
-  // put drawing code here
+	background(255);
+
+  var yoff = 0;
+
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+
+    for (var x = 0; x < cols; x++) {
+      var index = (x + y * width);
+
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 2;
+      var v = p5.Vector.fromAngle(angle);
+      
+    //   v.setMag(1);
+    //   flowfield[index] = v;
+    //   xoff += inc;
+    //   stroke(0, 50);
+
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      line(0, 0, scl, 0);
+      pop();
+
+      xoff += inc;
+    }
+    yoff += inc;
+
+    zoff += 0.0003;
+  }
+
+  // for (var i = 0; i < particles.length; i++) {
+  //   particles[i].follow(flowfield);
+  //   particles[i].update();
+  //   particles[i].edges();
+  //   particles[i].show();
+  // }
+
+  fr.html(floor(frameRate()));
 }
+
+
---
+++ test.js
@@ -0,0 +1,45 @@
+var inc = 0.1;
+var scl = 50; 
+var rows, cols; 
+
+var zoff = 0;
+var fr; 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+
+  fr = createP('')
+}
+
+function draw() {
+
+  background(255);
+  
+  var yoff = 0; 
+  for(var y = 0; y < rows; y++) { 
+    var xoff = 0; 
+
+    for(var x = 0; x < cols; x++) { 
+
+      var angle = noise(xoff, yoff, zoff) * TWO_PI; 
+      var v = p5.Vector.fromAngle(angle);
+
+      xoff += inc;
+
+      stroke(0);
+      push();
+      translate( x * scl, y * scl);
+      rotate(v.heading());
+      line(0, 0, scl, 0);
+
+      pop();
+    }
+    yoff += inc;
+    zoff += 0.005; 
+  }
+
+  fr.html(floor(frameRate()));
+} 
